e03cfaff54676c118ccf2cd43719283c5e84c84a
AnthonyScott <anthony.scott1005@gmail.com>
2023-02-27 14:32:46 -0500

    Board created

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
   /*----- constants -----*/
-
+const colors = {
+  empty: null,
+  player1: null,
+  player2: null,
+}
 
   /*----- state variables -----*/
 
 
   /*----- cached elements  -----*/
+let boardArr = document.querySelectorAll('.square');
 
 
   /*----- event listeners -----*/
 
 
   /*----- functions -----*/
+
+
+  console.log(boardArr);
+  boardArr[8].textContent = "heyo";
